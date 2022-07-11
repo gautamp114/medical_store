3c04c4047f637f47b93dc521cba67fa5f6a830bb
gautamp114 <gautamp114@gmail.com>
2022-07-11 22:04:29 +0545

    removed country,distict and province from user model to create superuser

--- accounts/models.py
+++ accounts/models.py
@@ -19,21 +19,11 @@ class CustomUser(AbstractUser):
     contact_number = PhoneNumberField(
         _("Mobile Number"), unique=True)
     sex = models.CharField(_("Gender"),choices=SEX, max_length=7, default="Male")
-    country = models.ForeignKey(Country,on_delete=models.CASCADE)
-    province = models.ForeignKey(Province,on_delete=models.CASCADE)
-    district = models.ForeignKey(District,on_delete=models.CASCADE)
-    # dob = models.DateField()
-    # address = models.CharField(max_length=50)
-    # business_type = models.CharField(max_length=10, choices=ROLE, default="1")
-
-    # USERNAME_FIELD = 'contact_number'
+    # country = models.ForeignKey(Country,on_delete=models.CASCADE)
+    # province = models.ForeignKey(Province,on_delete=models.CASCADE)
+    # district = models.ForeignKey(District,on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["id",]
-
-    @property
-    def is_admin(self):
-        return self.is_superuser
-        # return self.business_type == 1 or self.is_superuser
 
 
--- medical_store/settings.py
+++ medical_store/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'medical_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medical_store',
+        'USER': 'postgres',
+        'PASSWORD': 'pramod',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
